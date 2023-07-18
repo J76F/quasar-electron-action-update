13faa920beb07135ce1885a5dd66f70f51491b07
J76F <github@jfaber.nl>
2023-07-18 14:21:56 +0200

    fix

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -142,7 +142,7 @@ export default defineComponent({
       })
       window.electron.onAutoUpdateDownload((event, percent, message) => {
         this.autoUpdateDownloadMessage = message
-        this.autoUpdateDownloadPercent = math.round(percent)
+        this.autoUpdateDownloadPercent = Math.round(percent)
       })
     }
   }
