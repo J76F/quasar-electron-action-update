6ae4a41306e8c60f7902f590cb612181018674d6
Jeroen Faber <105970388+J76F@users.noreply.github.com>
2023-07-18 13:29:48 +0200

    Update MainLayout.vue

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -119,7 +119,7 @@ export default defineComponent({
   },
   computed: {
     indeterminateProgress () {
-      return autoUpdateDownloadMessage === 'downloading'
+      return this.autoUpdateDownloadMessage === 'downloading'
     }
   },
   setup () {
